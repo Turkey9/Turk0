caeb2fe0063a50121c4f35ddb20fe791d99b2968
Turkey9 <141520273+Turkey9@users.noreply.github.com>
2023-08-06 18:14:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("29028317"))
 API_HASH = getenv("8f2831819e4f3fdc44945d2e4d2aa095")
 
-BOT_TOKEN = getenv("6497603915:AAHiE5gfJbV3wZXnxoO-VKb3vJEMqMddVgs")
+BOT_TOKEN = getenv("6366225143:AAHDRljx5Rqp1K2pb9UHUBJJZYx6vouFGeE")
 OWNER_ID = int(getenv("5848055044"))
 
 MONGO_DB_URI = getenv("64cd0227652c571ae5e11041")
