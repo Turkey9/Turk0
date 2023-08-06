be21dbdbf9b0781e4150bcb90b1d18346c387e5c
Turkey9 <141520273+Turkey9@users.noreply.github.com>
2023-08-06 15:58:01 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("29028317"))
+API_HASH = getenv("8f2831819e4f3fdc44945d2e4d2aa095")
+
+BOT_TOKEN = getenv("6540492667:AAHhF4QSufK_VHwTwxUT749r0vQBiLoAkug")
+OWNER_ID = int(getenv("5848055044"))
+
+MONGO_DB_URI = getenv("64cd0227652c571ae5e11041")
+MUST_JOIN = getenv("@T_R_KY4", None)
